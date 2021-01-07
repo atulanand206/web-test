96e159a1a3651163869873b0f9fc0737e4b9d052
Atul Anand <atulanand206@gmail.com>
2021-01-07 20:59:52 +0530

    clean up

--- palette/src/components/App/App.jsx
+++ palette/src/components/App/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 import Container from "../Container/Container";
 
 function App() {
     return (
-        <div className="app-container">
-            <Container/>
-        </div>);
+        <Router>
+            <div className="app-container">
+                <Switch>
+                    <Route exact path='/' component={Container}/>
+                </Switch>
+            </div>
+        </Router>);
 }
 
 export default App;
